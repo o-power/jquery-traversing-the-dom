78c09a26673193bcdd1c2361c09816796b067798
Ubuntu <ubuntu@ip-172-31-80-74.ec2.internal>
2019-07-03 22:24:31 +0000

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(function() {
+   $("p").click(function() {
+      // makes the <p> element that you clicked on disappear
+      $(this).hide("slow");
+   });
+   
+   $("button").mouseenter(function() {
+      $(this).removeClass("").addClass("");
+   });
+});
