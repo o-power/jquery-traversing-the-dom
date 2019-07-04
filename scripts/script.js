067ac469291f1e64ac1da10b9815301fb21c4c57
Ubuntu <ubuntu@ip-172-31-80-74.ec2.internal>
2019-07-04 20:55:58 +0000

    Added more examples

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,61 @@
 $(function() {
-   $("p").click(function() {
+   //$("p").click(function() {
       // makes the <p> element that you clicked on disappear
-      $(this).hide("slow");
-   });
+   //   $(this).hide("slow");
+   //});
    
    $("button").mouseenter(function() {
-      $(this).removeClass("").addClass("");
+      // make this button turn from red to blue
+      $(this).removeClass("makeRed").addClass("makeBlue");
+   });
+   
+   $("button").mouseleave(function() {
+      // make this button turn from blue to red
+      $(this).removeClass("makeBlue").addClass("makeRed");
+   });
+   
+   // // alternative to using this to pick out a particular card
+   // $(".card").mouseenter(function() {
+   //    // array of class names
+   //    var classNames = $(this).attr("class").split(" "); // e.g. ["card","four"]
+   //    var cardClass = classNames[0];
+   //    var className = classNames[1];
+
+   //    if ($(this).css("background-color") == "rgb(255, 255, 255)") {
+   //       // apply an inline style
+   //       $("."+className).css("background-color","rgb(255, 255, 0)");   
+   //    }
+   // });
+   
+   // $(".card").mouseleave(function() {
+   //    // array of class names
+   //    var classNames = $(this).attr("class").split(" "); // e.g. ["card","four"]
+   //    var cardClass = classNames[0];
+   //    var className = classNames[1];
+
+   //    if ($(this).css("background-color") == "rgb(255, 255, 0)") {
+   //       // apply an inline style
+   //       $("."+className).css("background-color","rgb(255, 255, 255)");   
+   //    }
+   // });
+   
+   $(".card").click(function() {
+      $(this).toggleClass("highlight");
+   });
+   
+   // toggleClass()
+   // click to make font bigger, click again to go back to original size
+   $("h2").on("click", function() {
+      $(this).toggleClass("biggerFont");
+   });
+   
+   // hide buttons that are not highlighted
+   $("#select_btn").click(function() {
+      $(".card:not(.highlight)").hide("slow");
+   });
+   
+   // show all
+   $("#all_btn").click(function() {
+      $(".card").show();
    });
 });
